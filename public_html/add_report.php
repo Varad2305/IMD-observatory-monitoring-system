bdef164a98cd1c70f2545aa62e51043225a47afc
Varad2305 <f20170141@hyderabad.bits-pilani.ac.in>
2019-06-10 15:02:14 +0530

    Fully functional form

--- public_html/add_report.php
+++ public_html/add_report.php
@@ -14,16 +14,24 @@ if(!$set){
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Add Report</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Add Report</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<style type="text/css">
+    .bs-example{
+    	margin: 20px;
+    }
+</style>
 </head>
 <body>
 
-<div class="table-responsive">
-	<table class="table">
+
+	<table class="table table-dark">
 		<thead>
 			<tr>
 				<th>INSTRUMENT</th>
@@ -37,112 +45,110 @@ if(!$set){
 			<tr>
 					<td>Wind Wane</td>
 					<td><input type="radio" name="1" value=1></td>
-					<td><input type="radio" name="1" value=2></td>
+					<td><input type="radio" name="1" value=0></td>
 					<td><input type="radio" name="1" value=-1></td>
 			</tr>
 			<tr>
 					<td>Cup Counter Anemometer</td>
 					<td><input type="radio" name="2" value=1></td>
-					<td><input type="radio" name="2" value=2></td>
+					<td><input type="radio" name="2" value=0></td>
 					<td><input type="radio" name="2" value=-1></td>
 			</tr>
 			<tr>
 					<td>Max Thermometer</td>
 					<td><input type="radio" name="3" value=1></td>
-					<td><input type="radio" name="3" value=2></td>
+					<td><input type="radio" name="3" value=0></td>
 					<td><input type="radio" name="3" value=-1></td>
 			</tr>
 			<tr>
 					<td>Min Thermometer</td>
 					<td><input type="radio" name="4" value=1></td>
-					<td><input type="radio" name="4" value=2></td>
+					<td><input type="radio" name="4" value=0></td>
 					<td><input type="radio" name="4" value=-1></td>
 			</tr>
 			<tr>
 					<td>Dry Bulb Thermometer</td>
 					<td><input type="radio" name="5" value=1></td>
-					<td><input type="radio" name="5" value=2></td>
+					<td><input type="radio" name="5" value=0></td>
 					<td><input type="radio" name="5" value=-1></td>
 			</tr>
 			<tr>
 					<td>Wet Bulb Thermometer</td>
 					<td><input type="radio" name="6" value=1></td>
-					<td><input type="radio" name="6" value=2></td>
+					<td><input type="radio" name="6" value=0></td>
 					<td><input type="radio" name="6" value=-1></td>
 			</tr>
 			<tr>
 					<td>Stevenson Screen (single)</td>
 					<td><input type="radio" name="7" value=1></td>
-					<td><input type="radio" name="7" value=2></td>
+					<td><input type="radio" name="7" value=0></td>
 					<td><input type="radio" name="7" value=-1></td>
 			</tr>
 			<tr>
 					<td>Stevenson Screen (double)</td>
 					<td><input type="radio" name="8" value=1></td>
-					<td><input type="radio" name="8" value=2></td>
+					<td><input type="radio" name="8" value=0></td>
 					<td><input type="radio" name="8" value=-1></td>
 			</tr>
 			<tr>
 					<td>Thermograph</td>
 					<td><input type="radio" name="9" value=1></td>
-					<td><input type="radio" name="9" value=2></td>
+					<td><input type="radio" name="9" value=0></td>
 					<td><input type="radio" name="9" value=-1></td>
 			</tr>
 			<tr>
 					<td>Barograph</td>
 					<td><input type="radio" name="10" value=1></td>
-					<td><input type="radio" name="10" value=2></td>
+					<td><input type="radio" name="10" value=0></td>
 					<td><input type="radio" name="10" value=-1></td>
 			</tr>
 			<tr>
 					<td>Hydrograph</td>
 					<td><input type="radio" name="11" value=1></td>
-					<td><input type="radio" name="11" value=2></td>
+					<td><input type="radio" name="11" value=0></td>
 					<td><input type="radio" name="11" value=-1></td>
 			</tr>
 			<tr>
 					<td>Ordinary Rain Gauge</td>
 					<td><input type="radio" name="12" value=1></td>
-					<td><input type="radio" name="12" value=2></td>
+					<td><input type="radio" name="12" value=0></td>
 					<td><input type="radio" name="12" value=-1></td>
 			</tr>
 			<tr>
 					<td>Self Recording Rain Gauge</td>
 					<td><input type="radio" name="13" value=1></td>
-					<td><input type="radio" name="13" value=2></td>
+					<td><input type="radio" name="13" value=0></td>
 					<td><input type="radio" name="13" value=-1></td>
 			</tr>
 			<tr>
 					<td>Evaporimeter</td>
 					<td><input type="radio" name="14" value=1></td>
-					<td><input type="radio" name="14" value=2></td>
+					<td><input type="radio" name="14" value=0></td>
 					<td><input type="radio" name="14" value=-1></td>
 			</tr>
 			<tr>
 					<td>Mercury Barometer</td>
 					<td><input type="radio" name="15" value=1></td>
-					<td><input type="radio" name="15" value=2></td>
+					<td><input type="radio" name="15" value=0></td>
 					<td><input type="radio" name="15" value=-1></td>
 			</tr>
 			<tr>
 					<td>High Wind Speed Recorder</td>
 					<td><input type="radio" name="16" value=1></td>
-					<td><input type="radio" name="16" value=2></td>
+					<td><input type="radio" name="16" value=0></td>
 					<td><input type="radio" name="16" value=-1></td>
 			</tr>
-			<input type="submit" name="submit" value="Submit">
+			<tr><td><input type="submit" class="btn btn-primary" name="submit" value="Submit"></td></tr>
 			<?php
-				echo "Your input";
-				echo $_POST["1"];
-				echo $_POST["2"];
 				include('./utilities/db_connection.php');
-				$query = "INSERT INTO report(date_recorded,observatory,instrument,working) values(CURDATE(),'".$_SESSION["username"]."','Wind Wane','".$_POST["1"]."'),(CURDATE(),'".$_SESSION["username"]."','Cup Counter Anemometer','".$_POST["2"]."');";
+				$query = "INSERT INTO report(date_recorded,observatory,instrument,working) values(CURDATE(),'".$_SESSION["username"]."','Wind Wane','".$_POST["1"]."'),(CURDATE(),'".$_SESSION["username"]."','Cup Counter Anemometer','".$_POST["2"]."'),(CURDATE(),'".$_SESSION["username"]."','Max Thermometer','".$_POST["3"]."'),(CURDATE(),'".$_SESSION["username"]."','Min Thermometer','".$_POST["4"]."'),(CURDATE(),'".$_SESSION["username"]."','Dry Bulb Thermometer','".$_POST["5"]."'),(CURDATE(),'".$_SESSION["username"]."','Wet Bulb Thermometer','".$_POST["6"]."'),(CURDATE(),'".$_SESSION["username"]."','Stevenson Screen (single)','".$_POST["7"]."'),(CURDATE(),'".$_SESSION["username"]."','Stevenson Screen (double)','".$_POST["8"]."'),(CURDATE(),'".$_SESSION["username"]."','Thermograph','".$_POST["9"]."'),(CURDATE(),'".$_SESSION["username"]."','Barograph','".$_POST["10"]."'),(CURDATE(),'".$_SESSION["username"]."','Hydrograph','".$_POST["11"]."'),(CURDATE(),'".$_SESSION["username"]."','Ordinary Rain Gauge','".$_POST["12"]."'),(CURDATE(),'".$_SESSION["username"]."','Self Recording Rain Gauge','".$_POST["13"]."'),(CURDATE(),'".$_SESSION["username"]."','Evaporimeter','".$_POST["14"]."'),(CURDATE(),'".$_SESSION["username"]."','Mercury Barometer','".$_POST["15"]."'),(CURDATE(),'".$_SESSION["username"]."','High Wind Speed Recorder','".$_POST["16"]."');";
 				$c = getResult($query);
+				header("Location:station.php");
 			?>
 		</tbody>
 	</table>
 
 
-</div>
+
 </body>
 </html>
--- public_html/admin.php
+++ public_html/admin.php
@@ -112,23 +112,6 @@ if(!$set){
                     <button class="btn btn-dark d-inline-block d-lg-none ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <i class="fas fa-align-justify"></i>
                     </button>
-
-                    <!-- <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="nav navbar-nav ml-auto">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Page</a>
-                            </li>
-                        </ul>
-                    </div> -->
                 </div>
             </nav>
             <h2>Welcome Scientist <?php if($_SESSION["status"] == 1) echo 'F'; else echo 'E';?></h2><br></br>

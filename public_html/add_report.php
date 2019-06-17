e75d62300fc9aa805024e21350267926ecbb487c
Varad2305 <f20170141@hyderabad.bits-pilani.ac.in>
2019-06-17 10:30:58 +0530

    Added placeholders

--- public_html/add_report.php
+++ public_html/add_report.php
@@ -57,112 +57,112 @@ if(!$set){
 					<td><input type="radio" name="1" value=1></td>
 					<td><input type="radio" name="1" value=0></td>
 					<td><input type="radio" name="1" value=-1></td>
-					<td><input type="text" name="Wind_Wane"></td>
+					<td><input type="text" name="Wind_Wane" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Cup Counter Anemometer</td>
 					<td><input type="radio" name="2" value=1></td>
 					<td><input type="radio" name="2" value=0></td>
 					<td><input type="radio" name="2" value=-1></td>
-					<td><input type="text" name="Cup_Counter_Anemometer"></td>
+					<td><input type="text" name="Cup_Counter_Anemometer" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Max Thermometer</td>
 					<td><input type="radio" name="3" value=1></td>
 					<td><input type="radio" name="3" value=0></td>
 					<td><input type="radio" name="3" value=-1></td>
-					<td><input type="text" name="Max_Thermometer"></td>
+					<td><input type="text" name="Max_Thermometer" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Min Thermometer</td>
 					<td><input type="radio" name="4" value=1></td>
 					<td><input type="radio" name="4" value=0></td>
 					<td><input type="radio" name="4" value=-1></td>
-					<td><input type="text" name="Min_Thermometer"></td>
+					<td><input type="text" name="Min_Thermometer" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Dry Bulb Thermometer</td>
 					<td><input type="radio" name="5" value=1></td>
 					<td><input type="radio" name="5" value=0></td>
 					<td><input type="radio" name="5" value=-1></td>
-					<td><input type="text" name="Dry_Bulb_Thermometer"></td>
+					<td><input type="text" name="Dry_Bulb_Thermometer" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Wet Bulb Thermometer</td>
 					<td><input type="radio" name="6" value=1></td>
 					<td><input type="radio" name="6" value=0></td>
 					<td><input type="radio" name="6" value=-1></td>
-					<td><input type="text" name="Wet_Bulb_Thermometer"></td>
+					<td><input type="text" name="Wet_Bulb_Thermometer" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Stevenson Screen (single)</td>
 					<td><input type="radio" name="7" value=1></td>
 					<td><input type="radio" name="7" value=0></td>
 					<td><input type="radio" name="7" value=-1></td>
-					<td><input type="text" name="Stevenson_Screen_(Single)"></td>
+					<td><input type="text" name="Stevenson_Screen_(Single)" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Stevenson Screen (double)</td>
 					<td><input type="radio" name="8" value=1></td>
 					<td><input type="radio" name="8" value=0></td>
 					<td><input type="radio" name="8" value=-1></td>
-					<td><input type="text" name="Stevenson_Screen_(Double)"></td>
+					<td><input type="text" name="Stevenson_Screen_(Double)" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Thermograph</td>
 					<td><input type="radio" name="9" value=1></td>
 					<td><input type="radio" name="9" value=0></td>
 					<td><input type="radio" name="9" value=-1></td>
-					<td><input type="text" name="Thermograph"></td>
+					<td><input type="text" name="Thermograph" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Barograph</td>
 					<td><input type="radio" name="10" value=1></td>
 					<td><input type="radio" name="10" value=0></td>
 					<td><input type="radio" name="10" value=-1></td>
-					<td><input type="text" name="Barograph"></td>
+					<td><input type="text" name="Barograph" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Hydrograph</td>
 					<td><input type="radio" name="11" value=1></td>
 					<td><input type="radio" name="11" value=0></td>
 					<td><input type="radio" name="11" value=-1></td>
-					<td><input type="text" name="Hydrograph"></td>
+					<td><input type="text" name="Hydrograph" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Ordinary Rain Gauge</td>
 					<td><input type="radio" name="12" value=1></td>
 					<td><input type="radio" name="12" value=0></td>
 					<td><input type="radio" name="12" value=-1></td>
-					<td><input type="text" name="Ordinary_Rain_Gauge"></td>
+					<td><input type="text" name="Ordinary_Rain_Gauge" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Self Recording Rain Gauge</td>
 					<td><input type="radio" name="13" value=1></td>
 					<td><input type="radio" name="13" value=0></td>
 					<td><input type="radio" name="13" value=-1></td>
-					<td><input type="text" name="Self_Recording_Rain_Gauge"></td>
+					<td><input type="text" name="Self_Recording_Rain_Gaue" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Evaporimeter</td>
 					<td><input type="radio" name="14" value=1></td>
 					<td><input type="radio" name="14" value=0></td>
 					<td><input type="radio" name="14" value=-1></td>
-					<td><input type="text" name="Evaporimeter"></td>
+					<td><input type="text" name="Evaporimeter" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>Mercury Barometer</td>
 					<td><input type="radio" name="15" value=1></td>
 					<td><input type="radio" name="15" value=0></td>
 					<td><input type="radio" name="15" value=-1></td>
-					<td><input type="text" name="Mercury_Barometer"></td>
+					<td><input type="text" name="Mercury_Barometer" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 					<td>High Wind Speed Recorder</td>
 					<td><input type="radio" name="16" value=1></td>
 					<td><input type="radio" name="16" value=0></td>
 					<td><input type="radio" name="16" value=-1></td>
-					<td><input type="text" name="High_Wind_Speed_Recorder"></td>
+					<td><input type="text" name="High_Wind_Speed_Recorder" placeholder="Remark.."></td>
 			</tr>
 			<tr>
 				<td><a class="btn btn-primary" href="station.php" role="button">Go Back</a></td>
@@ -225,17 +225,6 @@ if(!$set){
 				else{
 					echo "<script type='text/javascript'> alert('Please fill all rows'); </script>";	
 				}
-
-
-				// $query = "INSERT INTO report(date_recorded,observatory,instrument,working,reviewed,remark) values(CURDATE(),'".$_SESSION["username"]."','Wind Wane','".$_POST["1"]."',0,'".$_POST["windwane"]."'),(CURDATE(),'".$_SESSION["username"]."','Cup Counter Anemometer','".$_POST["2"]."',0,'".$_POST["CCA"]."'),(CURDATE(),'".$_SESSION["username"]."','Max Thermometer','".$_POST["3"]."',0,'".$_POST["max_therm"]."'),(CURDATE(),'".$_SESSION["username"]."','Min Thermometer','".$_POST["4"]."',0,'".$_POST["min_therm"]."'),(CURDATE(),'".$_SESSION["username"]."','Dry Bulb Thermometer','".$_POST["5"]."',0,'".$_POST["DBT"]."'),(CURDATE(),'".$_SESSION["username"]."','Wet Bulb Thermometer','".$_POST["6"]."',0,'".$_POST["WBT"]."'),(CURDATE(),'".$_SESSION["username"]."','Stevenson Screen (single)','".$_POST["7"]."',0,'".$_POST["SSS"]."'),(CURDATE(),'".$_SESSION["username"]."','Stevenson Screen (double)','".$_POST["8"]."',0,'".$_POST["SSD"]."'),(CURDATE(),'".$_SESSION["username"]."','Thermograph','".$_POST["9"]."',0,'".$_POST["thermograph"]."'),(CURDATE(),'".$_SESSION["username"]."','Barograph','".$_POST["10"]."',0,'".$_POST["barograph"]."'),(CURDATE(),'".$_SESSION["username"]."','Hydrograph','".$_POST["11"]."',0,'".$_POST["hydrograph"]."'),(CURDATE(),'".$_SESSION["username"]."','Ordinary Rain Gauge','".$_POST["12"]."',0,'".$_POST["ORG"]."'),(CURDATE(),'".$_SESSION["username"]."','Self Recording Rain Gauge','".$_POST["13"]."',0,'".$_POST["SRRG"]."'),(CURDATE(),'".$_SESSION["username"]."','Evaporimeter','".$_POST["14"]."',0,'".$_POST["evaporimeter"]."'),(CURDATE(),'".$_SESSION["username"]."','Mercury Barometer','".$_POST["15"]."',0,'".$_POST["MB"]."'),(CURDATE(),'".$_SESSION["username"]."','High Wind Speed Recorder','".$_POST["16"]."',0,'".$_POST["HWSR"]."');"; 
-				// // $query = "INSERT INTO report(date_recorded,observatory,instrument,working,reviewed,remark) VALUES(CURDATE(),'".$_SESSION["username"]."','Wind Wane','".$_POST["1"]."',0,'".$_POST["windwane"]."');";
-				// $c = getResult($query);
-				// if($c === FALSE){
-				// 	echo "<script type='text/javascript'> alert('Error'); </script>";
-				// }
-				// else{
-				// 	echo "<script type='text/javascript'> alert('Success'); </script>";
-				// }
 			}
 			?>
 		</tbody>
@@ -249,11 +238,11 @@ if(!$set){
 		var cell3 = row.insertCell(2);
 		var cell4 = row.insertCell(3);
 		var cell5 = row.insertCell(4);
-		cell1.innerHTML = "<input type='text' name='addedt'>";
+		cell1.innerHTML = "<input type='text' name='addedt' placeholder='Instrument..'>";
 		cell2.innerHTML = "<input type='radio' name='addedr'>";
 		cell3.innerHTML = "<input type='radio' name='addedr'>";
 		cell4.innerHTML = "<input type='radio' name='addedr'>";
-		cell5.innerHTML = "<input type='text' name='addedremark'>";
+		cell5.innerHTML = "<input type='text' name='addedremark' placeholder='Remark..'>";
 	}
 
 </script>
